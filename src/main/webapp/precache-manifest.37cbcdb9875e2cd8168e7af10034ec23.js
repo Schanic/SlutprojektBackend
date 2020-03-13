4ac41ab04e2c21090d11586be7c91561ad89660a
Schanic <43879948+Schanic@users.noreply.github.com>
2020-03-13 13:09:58 +0100

    aawd

---
+++ src/main/webapp/precache-manifest.37cbcdb9875e2cd8168e7af10034ec23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96bf12077e5647649dc",
+    "url": "./static/css/main.e0c812ee.chunk.css"
+  },
+  {
+    "revision": "e96bf12077e5647649dc",
+    "url": "./static/js/main.9b475fac.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "2a24f37161e67b93f4b6",
+    "url": "./static/css/2.bbf14060.chunk.css"
+  },
+  {
+    "revision": "2a24f37161e67b93f4b6",
+    "url": "./static/js/2.07d39ef0.chunk.js"
+  },
+  {
+    "revision": "1a6150de8bc2008f1c7535e824191fd7",
+    "url": "./index.html"
+  }
+];
